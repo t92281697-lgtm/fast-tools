ad139b9b966e3c2b756148abef37da75e5d00679
t92281697-lgtm <t92281697@gmail.com>
2026-07-07 13:48:53 +0900

    Add QR Code Generator

--- tools/qr-generator/script.js
+++ tools/qr-generator/script.js
@@ -1,1 +1,19 @@
+function makeQR() {
 
+    const text = document.getElementById("text").value.trim();
+
+    if (text === "") {
+        alert("文字やURLを入力してください。");
+        return;
+    }
+
+    const qr = document.getElementById("qrcode");
+    qr.innerHTML = "";
+
+    new QRCode(qr, {
+        text: text,
+        width: 250,
+        height: 250
+    });
+
+}
